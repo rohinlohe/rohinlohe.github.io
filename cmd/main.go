013346a121a1dd649270ce8bbb177768632255f6
rohinlohe <rohinlohe@users.noreply.github.com>
2019-11-10 20:48:42 -0800

    simple endpoint

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/blog", blogHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
